20dc6b75fc689e363b6d0f2ca60cd1efad3411ff
Vaibhav Singh Kalura <kaluravaibhav@gmail.com>
2025-12-07 14:57:47 +0530

    Initial commit

---
+++ src/main/java/com/springboot/vaibhav/module1introduction/Module1introductionApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.vaibhav.module1introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module1introductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module1introductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/vaibhav/module1introduction/Module1introductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.vaibhav.module1introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module1introductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
